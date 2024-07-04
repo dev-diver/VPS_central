f143065f2df6ce264f12817bb1c4a7ccadabd8fc
SoKyungHyun <valentine92exp@gmail.com>
2024-07-04 17:49:21 +0900

    docker-compose-update shell script

---
+++ docker-compose-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "sudo로 실행해주세요"
+  exit 1
+fi
+
+PROJECT_DIR=$(dirname $(realpath $0))
+
+cd $PROJECT_DIR
+
+docker-compose pull
+
+docker-compose up -d
