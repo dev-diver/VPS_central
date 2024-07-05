16b0c9f1ed47618800cf2a3f6c887d47193b0feb
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-07-05 17:37:01 +0900

    client, server 재시작

--- docker-compose-update.sh
+++ docker-compose-update.sh
@@ -13,8 +13,20 @@ cd $PROJECT_DIR
 echo "git pull"
 git pull origin main
 
-echo "docker compose pull!"
-docker-compose pull
+# 클라이언트, 서버 중지 및 제거
+docker-compose stop client server
+docker-compose rm -f client server
 
-exho "docker compose up"
-docker-compose up -d
+# 기존 볼륨 데이터 제거
+VOLUME_NAME="${PROJECT_NAME}_front_web"
+VOLUME_PATH=$(docker volume inspect --format '{{ .Mountpoint }}' $VOLUME_NAME)
+sudo rm -rf "$VOLUME_PATH"/*
+
+echo "docker compose pull!"
+docker-compose pull || { echo "docker-compose pull 실패"; exit 1; }
+
+echo "docker compose up"
+docker-compose up -d client server
+docker-compose up -d client server || { echo "docker-compose up 실패"; exit 1; }
+
+echo "script 완료"
