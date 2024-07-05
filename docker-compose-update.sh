e704174a34964cc4b98db17d9fb943913e4e56fb
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-07-05 15:58:11 +0900

    add echo on script

--- docker-compose-update.sh
+++ docker-compose-update.sh
@@ -5,12 +5,16 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+echo "스크립트 시작"
 PROJECT_DIR=$(dirname $(realpath $0))
 
 cd $PROJECT_DIR
 
+echo "git pull"
 git pull origin main
 
+echo "docker compose pull"
 docker-compose pull
 
+exho "docker compose up"
 docker-compose up -d
