3a79fe6827bf957d47f12f10b6d760282a2b14c2
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-07-05 16:02:13 +0900

    get log

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ WORKDIR /app
 # Copy the current directory contents into the container at /app
 COPY . /app
 
-# Install any needed packages specified in requirements.txt
-RUN pip install --no-cache-dir Flask
+# # Install any needed packages specified in requirements.txt
+# RUN pip install --no-cache-dir Flask
 
 # Make port 5000 available to the world outside this container
 EXPOSE 5000
@@ -16,5 +16,5 @@ EXPOSE 5000
 # Define environment variable
 ENV NAME World
 
-# Run app.py when the container launches
-CMD ["python", "app/webhook-server.py"]
+# # Run app.py when the container launches
+# CMD ["python", "app/webhook-server.py"]
--- app/webhook-server.py
+++ app/webhook-server.py
@@ -14,7 +14,20 @@ def webhook():
             root_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
             script_path = os.path.join(root_dir, 'docker-compose-update.sh')
             # Run the script to pull and restart the containers
-            subprocess.Popen([script_path])
+            log_file_path = os.path.join(root_dir, 'update.log')
+            
+            with open(log_file_path, 'a') as log_file:
+                log_file.write(f"Webhook triggered at {current_dir}\n")
+                
+                # Run the script to pull and restart the containers
+                process = subprocess.run(['sh', script_path], stdout=log_file, stderr=log_file)
+                
+                # Check for errors
+                if process.returncode != 0:
+                    log_file.write(f"Script failed with return code {process.returncode}\n")
+                else:
+                    log_file.write("Script executed successfully\n")
+                    
         return 'Success', 200
     else:
         return 'Method Not Allowed', 405
